982dc0aad25e298c882a09634ec572834bc06038
Nathan Gilbert <nlg@iastate.edu>
2021-12-02 01:41:19 -0600

    Completed days 1 and 2

---
+++ day1/main.py
@@ -0,0 +1,37 @@
+from errno import ENOENT
+from os import strerror
+from os.path import exists
+from typing import List
+
+def load_input(path: str) -> List[int]:
+    '''Loads the input and returns it as a list of (PasswordPolicy, str) tuples'''
+    if not exists(path):
+        raise FileNotFoundError(ENOENT, strerror(ENOENT), path)
+    
+    with open(path, 'r') as input_file:
+        str_data = input_file.readlines()
+        int_data = [int(line.strip()) for line in str_data]
+        return int_data
+
+def count_increases(data: List[int], span: int = 1) -> int:
+    '''Counts the number of times a value in the data is greater than the preceding value.'''
+    num_increases = 0
+    for first, second in zip(data[:-span], data[span:]):
+        if second > first:
+            num_increases += 1
+    return num_increases
+
+def main():
+    # Load in the data
+    data = load_input('day1/puzzle_input.txt')
+    
+    print('--- Part 1 ---')
+    print(f'The depth increases {count_increases(data)} times.')
+    print('')
+
+    print('--- Part 2 ---')
+    print(f'The three-sized sliding window increases {count_increases(data, 3)} times.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ day2/main.py
@@ -0,0 +1,67 @@
+from errno import ENOENT
+from os import strerror
+from os.path import exists
+from typing import List, Tuple
+
+def load_input(path: str) -> List[int]:
+    '''Loads the input and returns it as a list of (PasswordPolicy, str) tuples'''
+    if not exists(path):
+        raise FileNotFoundError(ENOENT, strerror(ENOENT), path)
+    
+    with open(path, 'r') as input_file:
+        return [line.strip() for line in input_file.readlines()]
+
+def apply_steps_naive(steps: List[str]) -> Tuple[int, int]:
+    '''Applies the list of steps consecutively, returning the final position.'''
+    pos_h = 0
+    pos_d = 0
+    for step in steps:
+        tokens = step.split(' ')
+        direction = tokens[0]
+        magnitude = int(tokens[1])
+        if direction == 'forward':
+            pos_h += magnitude
+        elif direction == 'up':
+            pos_d -= magnitude
+        elif direction == 'down':
+            pos_d += magnitude
+    return (pos_h, pos_d)
+
+def apply_steps_with_aim(steps: List[str]) -> Tuple[int, int]:
+    '''Applies the list of steps consecutively, returning the final position.'''
+    pos_h = 0
+    pos_d = 0
+    aim = 0
+    for step in steps:
+        tokens = step.split(' ')
+        direction = tokens[0]
+        magnitude = int(tokens[1])
+        if direction == 'forward':
+            pos_h += magnitude
+            pos_d += aim * magnitude
+        elif direction == 'up':
+            aim -= magnitude
+        elif direction == 'down':
+            aim += magnitude
+    return (pos_h, pos_d)
+
+
+def main():
+    # Load in the data
+    data = load_input('day2/puzzle_input.txt')
+    
+    print('--- Part 1 ---')
+    pos_h, pos_d = apply_steps_naive(data)
+    print(f'Final position is horizontal: {pos_h}, depth: {pos_d}')
+    print(f'The product of our position components is {pos_h * pos_d}')
+    print('')
+
+    print('--- Part 2 ---')
+    pos_h, pos_d = apply_steps_with_aim(data)
+    print(f'Final position is horizontal: {pos_h}, depth: {pos_d}')
+    print(f'The product of our position components is {pos_h * pos_d}')
+    
+
+
+if __name__ == '__main__':
+    main()
